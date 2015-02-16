eed8c41ab86ace0d452ff16e27257de8f10551a6
niky <niky@asus>
2015-02-16 22:52:23 +0100

    Iterazione 1 Refactored: rimozione UserConnection e sistemazione UserController

--- src/Snuc/UserController.java
+++ src/Snuc/UserController.java
@@ -1,30 +1,41 @@
 package Snuc;
 
-import Common.Command;
+import Common.IMessagingService;
 import Common.IUser;
 import Common.IUser_Interaction;
-import Common.Notify;
 import Common.PublicNotify;
 import Common.TypeNotify;
+import Connector.ConnectionFactory;
+import Connector.ProxyMessagingService;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.GregorianCalendar;
 
 /**
  * La classe rappresenta il controller dell'utente
- *
+ * 
  * @author Russo Leandro, Invincibile Daniele e Didomenico Nicola
  */
 public class UserController implements IUser {
 
     private User user;
     private IUser_Interaction view;
-    private UserConnection connection;
-
-    public UserController() {
-    }
+    private IMessagingService server;
+    
 
     /**
+     * Il metodo imposta il riferimento all'oggetto Utente a cui si riferisce 
+     * il controller.
+     * 
+     * @param user utente
+     */
+    public void setUser(User user){
+        this.user=user;
+    }
+    
+    /**
      * Il metodo imposta il riferimento all'interfaccia utente.
-     *
+     * 
      * @param v vista
      */
     public void setView(IUser_Interaction v) {
@@ -32,77 +43,95 @@ public class UserController implements IUser {
     }
 
     /**
-     * Il metodo imposta il riferimento all'oggetto Utente a cui si riferisce il
-     * controller.
-     *
-     * @param user utente
-     */
-    public void setUser(User user) {
-        this.user = user;
-    }
-
-    /**
      * Il metodo si occupa della connessione del client con il server
-     *
+     * 
      * @param nick nickname dell'utente
      * @param address indirizzo del server
      * @param port porta in cui è in ascolto il server
-     * @return <code>true</code> se la connesione va a buon fine
-     * <code>false</code> se la connessione fallisce
+     * @throws UnknownHostException l'eccezione viene lanciata nel caso in cui il
+     * nome dell'host non viene riconosciuto
+     * @throws Exception l'eccezione viene lanciata nel caso in cui non è stato possibile
+     * instaurare la connessione
      */
-    public boolean connect(String nick, String address, int port) {
-        try {
-            connection = new UserConnection(address, port, this);
+    public void connect(String nick, String address, int port) throws UnknownHostException,Exception{
             user.setNick(nick);
-            String confirmNick = connection.connect(user.getNick());
-            user.setNick(confirmNick);
-            return true;
-        } catch (Exception e) {
-            System.out.println("Connection error!!");
-            e.printStackTrace();
-            return false;
-        }
-    }
+            server=ConnectionFactory.getConnectionFactory().createProxyMessagingSevice();
+            ProxyMessagingService service=(ProxyMessagingService)server;
+            service.setAddress(InetAddress.getByName(address));
+            service.setPort(port);
+            service.connect(nick,this);            
+    }        
 
-    /**
-     * Il metodo permette l'invio di un comando al server per richiedere un
-     * determinato servizio
-     *
-     * @param cmd contenuto del comando
-     */
-    public void executeCommand(String cmd) {
-        Command command = new Command(cmd, null, user.getNick(), null);
-        connection.senderMessage(command);
-    }
-
-//--------------------------Metodi dell'interfaccia IUser -------------------------------------------------    
-    @Override
-    public void receiveNotify(
-            TypeNotify          type,
-            String              content,
-            GregorianCalendar   calendar,
-            String              sender
-    ){};
-    
-    @Override
-    public  void receivePublicNotify(
-            TypeNotify          type,
-            String              content,
-            GregorianCalendar   calendar,
-            String              sender,
-            String              roomName
-    ) {};
-
-
-
-//---------------------------------------------------------------------------------------------------------    
     /**
      * Il metodo si occupa della gestione dei messaggi di log
-     *
+     * 
      * @param log messaggi di log
      */
     public void logHandler(String log) {
         view.printLog(log);
     }
+    
 
+    
+    /**
+     * Il metodo permette l'invio di un comando al server per richiedere un
+     * determinato servizio
+     * 
+     * @param cmd contenuto del comando
+     */
+    public void executeCommand(String cmd) {
+        server.commandHandler(cmd, user.getNick());        
+    }
+    
+
+
+
+    
+ //----------------Medoti dell'interfaccia IUser -----------------------------------   
+    @Override
+    public void receiveNotify(
+            TypeNotify type,
+            String content,
+            GregorianCalendar calendar,
+            String sender) {
+        switch (type){
+            case UPDATE_LIST_ROOMS:
+                view.updateRoomList(content);
+                break;
+            case BAD_COMMAND:
+                view.printLog(content);
+                break;
+            case CONNECTION_ACCEPT:
+                user.setNick(sender);
+                view.printLog(content);
+                break;
+        }
+    }
+    
+    @Override
+    public void receivePublicNotify(
+            TypeNotify type,
+            String content,
+            GregorianCalendar calendar,
+            String sender,
+            String roomName) {
+        switch (type) {
+            case UPDATE_LIST_USERS:
+                String users[] = content.split("\n");
+                user.updateListRoom(roomName, users);
+                view.updateUsersToRoom(roomName, users);
+                break;
+            case ADD_USER_TO_ROOM:
+                String log=PublicNotify.textFormat(type,null,calendar, sender,roomName);
+                view.printLogRoom(log,roomName);
+                break;
+        }
+    }
+    
+    
+
+    
+   
+    
+    //----------------------------------------------------------------------------------
 }
