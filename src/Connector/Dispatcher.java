68a4114e6058bad5737236272440ad900e124ff1
Spider <Leandro@192.168.1.105>
2015-02-25 18:43:54 +0100

    Iterazione 2 Package Connector aggiunta messaggio pubblic al Dispatcher

--- src/Connector/Dispatcher.java
+++ src/Connector/Dispatcher.java
@@ -1,3 +1,21 @@
+/**
+ * SNUC  is a program written in Java SE (version 1.8.0_31) during a project of 
+ * course Software Engineering in University of Catania academic year 2014-15.
+ * SNUC is Smart Network University Communications.
+ * 
+ * Copyright (C) 2015 onwards Leandro Russo (leandrorusso90@gmail.com)
+ * Copyright (C) 2015 onwards Invincibile Daniele (d.invincibile@gmail.com)
+ * Copyright (C) 2015 onwards Nicola Didomenico (nicola.didomenico@gmail.com)
+ * This program is free software: you can redistribute it and/or modify it under 
+ * the terms of the GNU General Public License as published by the Free Software 
+ * Foundation, either version 3 of the License, or (at your option) any later 
+ * version.
+ * This program is distributed in the hope that it will be useful, but WITHOUT 
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+ * FOR A PARTICULAR PURPOSE.See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public Licens along with 
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 package Connector;
 
 
@@ -6,6 +24,7 @@ import Common.IMessagingService;
 import Common.IUser;
 import Common.Message;
 import Common.Notify;
+import Common.PublicMessage;
 import Common.PublicNotify;
 import Common.TypeNotify;
 import java.util.GregorianCalendar;
@@ -71,6 +90,14 @@ public abstract class Dispatcher implements Runnable{
         else if (msg instanceof Notify){
             TypeNotify type = ((Notify) msg).getNotify();
             user.receiveNotify(type, content, date, sender);
+        }
+        else if ((msg instanceof PublicMessage)&&(user!=null)){
+            String room = ((PublicMessage) msg).getRoom();
+            user.receivePublicMessage(room, content,date, sender);
+        }
+        else if ((msg instanceof PublicMessage)&&(messagingService!=null)){
+            String room = ((PublicMessage) msg).getRoom();
+            messagingService.publicMessage(content,sender,room);
         }
         else if (msg instanceof Command) {
             messagingService.commandHandler(content, sender);
